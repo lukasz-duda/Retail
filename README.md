26ed52fb61c5e7e2a7494dec4430cdcc834911c9
Łukasz Duda <lukasz.duda.pl@gmail.com>
2022-10-16 19:28:51 +0200

    Connect to RabbitMQ

---
+++ README.md
@@ -0,0 +1,3 @@
+# Retail
+
+Asynchronous message-based communication.
---
+++ Sales/Program.cs
@@ -0,0 +1,25 @@
+﻿using NServiceBus;
+
+class Program
+{
+    static void Main()
+    {
+        AsyncMain().GetAwaiter().GetResult();
+    }
+
+    static async Task AsyncMain()
+    {
+        Console.WriteLine("Sales");
+        var endpointConfiguration = new EndpointConfiguration("Sales");
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+        transport.ConnectionString("host=localhost");
+        transport.UseConventionalRoutingTopology(QueueType.Quorum);
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        
+        Console.ReadLine();
+
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
